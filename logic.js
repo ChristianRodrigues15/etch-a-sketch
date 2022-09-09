d4c8be891367ef5476003190ab1b76186db4daff
Christian Rodrigues <christianrodrigues1515@gmail.com>
2022-09-09 11:39:59 -0400

    Added function to create divs inside the main container. Attempted adding functionality to some buttons

---
+++ logic.js
@@ -0,0 +1,24 @@
+const mainDiv = document.querySelector(".main")
+const blackBtn = document.querySelector(".buttonBlack")
+const grayBtn = document.querySelector(".buttonGray")
+const rainbowBtn = document.querySelector(".buttonRbw")
+const clear = document.querySelector(".buttonClear")
+
+
+
+function createDivs(rows, cols) {
+    mainDiv.style.setProperty('--grid-rows', rows);
+    mainDiv.style.setProperty('--grid-cols', cols);
+    for(i = 0; i < (rows * cols); i++) {
+        const newDiv = document.createElement("div")
+        newDiv.id = "canvas"
+        mainDiv.appendChild(newDiv)
+    }
+}
+
+blackBtn.addEventListener('click', () => {
+    newDiv.style.backgroundColor = "black";
+})
+
+
+createDivs(20, 20)
